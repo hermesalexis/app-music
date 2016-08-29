43e1c6830fa109aab57a72464e35fe7e37d11725
hermesalexis <hermesagm@gmail.com>
2016-08-29 12:38:50 -0500

    Crea funcionalidad para crear nuevo artista rutas get y post

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,22 @@
 class ArtistsController < ApplicationController
   def index
   end
+
+  def new
+   @artist = Artist.new
+  end
+
+  def create
+  	@artist = Artist.new(artist_params)
+  	if @artist.save
+  		redirect_to artists_path, notice: "El Artista fue publicado"
+  	else
+  		render :new
+  	end
+  end
+
+  private
+    def artist_params
+      params.required(:artist).permit(:name, :image_url)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/artists' , to: 'artists#index'
-
+  get '/artists/new', to: 'artists#new', as: 'new_artist'
+  post '/artists' , to: 'artists#create'
   
 end
